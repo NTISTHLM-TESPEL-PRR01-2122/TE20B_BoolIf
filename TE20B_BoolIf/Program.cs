429375154768c383683584e965703130b542fe96
Mikael Bergström <krank23@gmail.com>
2021-09-08 10:43:46 +0200

    Initial commit

---
+++ TE20B_BoolIf/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// string name = "6";
+// Console.WriteLine(name + name);
+
+Console.WriteLine("Vad heter du?");
+string name = Console.ReadLine();
+
+name = name.ToLower();
+
+//if (name == "micke" || name == "Micke" || name == "mIckE")
+
+if (name == "micke")
+{
+  Console.WriteLine("Welcome!");
+}
+else if (name == "albert")
+{
+  Console.WriteLine("Almost as welcome but not quite");
+}
+else
+{
+  Console.WriteLine("NO");
+}
+
+Console.ReadLine();
